da7d6b3809efbdb4aed37f2b2a10348b66d3bf22
Gabe <jflores353@gmail.com>
2019-04-17 14:38:23 -0500

    Initially created GET route to retrieve all items. Now created POST route

--- routes/api/items.js
+++ routes/api/items.js
@@ -11,6 +11,18 @@ router.get('/', (req, res) => {
     Item.find()
         .sort({ date: -1 })
         .then(items => res.json(items))
-})
+});
+
+// @route POST api/items
+// @desc Create a Post
+// @access Public
+// Following constructs an object to insert into the DB
+router.get('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+    // Now save this variable to the DB
+    newItem.save().then(item => res.json(item));
+});
 
 module.exports = router;
