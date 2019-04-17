54726ff9de43ed1a82cce3fed2a05248d38658a7
Gabe <jflores353@gmail.com>
2019-04-17 15:04:13 -0500

    added Delete request path

--- routes/api/items.js
+++ routes/api/items.js
@@ -25,4 +25,13 @@ router.get('/', (req, res) => {
     newItem.save().then(item => res.json(item));
 });
 
+// @route DELETE api/items/:id
+// @desc Delete A Item
+// @access Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }));
+});
+
 module.exports = router;
