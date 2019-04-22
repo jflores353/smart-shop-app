420874cef787fe37f1f5328dfdab6cd0f5020f12
Gabe <jflores353@gmail.com>
2019-04-21 23:02:32 -0500

    added reducer for ITEMS_LOADING

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,13 +1,8 @@
-import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'Eggs' },
-        { id: uuid(), name: 'Milk' },
-        { id: uuid(), name: 'Steak' },
-        { id: uuid(), name: 'Water' }
-    ]
+    items:[],
+    loading: false
 }
 
 export default function(state = initialState, action) {
@@ -25,6 +20,11 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 items: state.items.filter(item => item.id !== action.payload)
+            };
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
             };
         default: 
             return state;
