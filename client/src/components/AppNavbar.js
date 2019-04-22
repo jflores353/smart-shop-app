75bfc7f588679648bb7cd710a440ac933ce0608c
Gabe <jflores353@gmail.com>
2019-04-22 12:58:16 -0500

    changed background color

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -26,7 +26,7 @@ class AppNavbar extends Component {
     render() {
         return(
             <div>
-                <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Navbar color="primary" dark expand="sm" className="mb-5">
                     <Container>
                         <NavbarBrand href="/">ShoppingList</NavbarBrand>
                         <NavbarToggler onClick={this.toggle} />
