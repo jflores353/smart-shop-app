44fe7907ebe2c282452ff4ea832d899c1b42ddde
Gabe <jflores353@gmail.com>
2019-04-22 12:47:27 -0500

    added underscore before id for DELETE_ITEM to work with mongoDB

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -3,7 +3,7 @@ import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/type
 const initialState = {
     items:[],
     loading: false
-}
+};
 
 export default function(state = initialState, action) {
     switch(action.type) {
@@ -13,21 +13,25 @@ export default function(state = initialState, action) {
                 items: action.payload,
                 loading: false
             };
+
         case ADD_ITEM:
             return {
                 ...state,
                 items: [action.payload, ...state.items]
             };
+
         case DELETE_ITEM:
             return {
                 ...state,
-                items: state.items.filter(item => item.id !== action.payload)
+                items: state.items.filter(item => item._id !== action.payload)
             };
+        
         case ITEMS_LOADING:
             return {
                 ...state,
                 loading: true
             };
+
         default: 
             return state;
     }
