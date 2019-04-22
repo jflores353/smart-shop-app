f30f63cfc8c4db0d279e6e3bf0654d9aba11b243
Gabe <jflores353@gmail.com>
2019-04-21 22:12:54 -0500

    removed add Item button and this has been moved to ItemModal filein components

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { getItems, deleteItem }from '../actions/itemActions';
 import PropTypes from 'prop-types';
@@ -20,20 +19,6 @@ class ShoppingList extends Component {
         const { items } =this.props.item; // This is where our state lives currently
         return(
             <Container>
-                <Button
-                    color="dark"
-                    style={{ marginBottom: '2rem' }}
-                    onClick={() => {
-                        const name = prompt('Enter Item');
-                        if (name) {
-                            this.setState(state => ({
-                                items: [...state.items, { id: uuid(), name}]
-                            }));
-                        }
-                    }}
-                >Add Item
-                </Button>
-
                 <ListGroup>
                     <TransitionGroup className="shopping-list">
                         {items.map(({ id, name }) => (
