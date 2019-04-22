49a7268dba2efbf0baabf0320a7cb7ae2b451522
Gabe <jflores353@gmail.com>
2019-04-21 21:32:10 -0500

    Client side folders and files added. Created actions and reducers folders. Connected mapping of state object using redux.

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,8 @@
+// This is where we make our requests to the backend
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
+
+export const getItems = () => {
+    return {
+        type: GET_ITEMS
+    };
+};
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+    Collapse, // Allows Toggler
+    Navbar,
+    NavbarToggler, // Hamburger menu, will use this.toggle
+    NavbarBrand, // Can have a href attribute
+    Nav, // Wraps around all the links
+    NavItem, // Wraps the NavLink
+    NavLink, // Will have the href attribute
+    Container // Bootstrap container to move everything to the middle
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+            isOpen: false
+        }
+
+    
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/">ShoppingList</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="https://github.com/jflores353">
+                                        Github
+                                    </NavLink>     
+                                </NavItem>  
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+
+
+export default AppNavbar;
