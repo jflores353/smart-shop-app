92636419f5441b90136e24723307041cd6930998
Gabe <jflores353@gmail.com>
2019-04-21 21:53:07 -0500

    created onDeleteClick method for Remove item button

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -3,13 +3,17 @@ import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
 import { connect } from 'react-redux';
-import { getItems }from '../actions/itemActions';
+import { getItems, deleteItem }from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
 
     componentDidMount() {
         this.props.getItems();
+    }
+
+    onDeleteClick = id => {
+        this.props.deleteItem(id);
     }
    
     render(){
@@ -39,11 +43,7 @@ class ShoppingList extends Component {
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
-                                        onClick={() => {
-                                            this.setState(state => ({  
-                                                items: state.items.filter(item => item.id !== id)
-                                            }));
-                                        }}
+                                        onClick={this.onDeleteClick.bind(this, id)}
                                     >&times;</Button>
                                     {name}
                                 </ListGroupItem>
@@ -67,4 +67,4 @@ const mapStateToProps = (state) => ({
     item: state.item
 });
 
-export default connect(mapStateToProps, { getItems })(ShoppingList);
+export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
