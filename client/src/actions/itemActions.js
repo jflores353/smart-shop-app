50919dd50f0179875c4a04a7f4b2a72a97193a37
Gabe <jflores353@gmail.com>
2019-04-21 22:28:51 -0500

    added action for addItem

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -7,6 +7,13 @@ export const getItems = () => {
     };
 };
 
+export const addItem = item => {
+    return {
+        type: ADD_ITEM,
+        payload: item
+    };
+};
+
 export const deleteItem = id => {
     return {
         type: DELETE_ITEM,
