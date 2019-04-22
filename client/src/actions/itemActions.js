7b927a93ca2768b73a249abe73d5d77234127f42
Gabe <jflores353@gmail.com>
2019-04-22 12:35:37 -0500

    added dispatch for deleteItem

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
 
 export const getItems = () => dispatch => {
+    // return {
+    //  type: GET_ITEMS
+    // }
     dispatch(setItemsLoading());
     axios
         .get('/api/items')
@@ -11,21 +14,29 @@ export const getItems = () => dispatch => {
                 type: GET_ITEMS,
                 payload: res.data
             })
+        );
+};
+
+export const addItem = item => dispatch => {
+    axios
+        .post('/api/items', item)
+        .then(res =>
+            dispatch({
+                type: ADD_ITEM,
+                payload: res.data
+            })
         )
 };
 
-export const addItem = item => {
-    return {
-        type: ADD_ITEM,
-        payload: item
-    };
-};
-
-export const deleteItem = id => {
-    return {
-        type: DELETE_ITEM,
-        payload: id
-    };
+export const deleteItem = id => dispatch => {
+    axios
+        .delete(`/api/item/${id}`)
+        .then(res =>
+            dispatch({
+                type: DELETE_ITEM,
+                payload: id
+            })
+        );
 };
 
 export const setItemsLoading = () => {
