5b64810acb24f6de091ee26c88651f39a42ed6cc
Gabe <jflores353@gmail.com>
2019-04-25 14:56:47 -0500

    created mapsContainer to use google maps api for grocery stores map

---
+++ client/src/components/Maps.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    componentDidMount() {
+      let map = new window.google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.8688, lng: 151.2195},
+        zoom: 13,
+        mapTypeId: 'roadmap',
+      });
+    }
+  
+    render() {
+      return (
+        <div id='app'>
+          <div id='map' />
+        </div>
+      ); 
+    } 
+  };
