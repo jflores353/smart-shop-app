75880fe8df4aac0149ea7c30ca6fbdc73e170123
Gabe <jflores353@gmail.com>
2019-04-25 21:22:48 -0500

    Updates

---
+++ precache-manifest.4d44f5d8b8f52c5f52a2716a2508d6c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a25d5f3b21293ce311bd",
+    "url": "/smart-shop-app/static/css/main.fa4a1fde.chunk.css"
+  },
+  {
+    "revision": "a25d5f3b21293ce311bd",
+    "url": "/smart-shop-app/static/js/main.3e21dbc0.chunk.js"
+  },
+  {
+    "revision": "37ad1344386db7303d7b",
+    "url": "/smart-shop-app/static/js/runtime~main.42978bc2.js"
+  },
+  {
+    "revision": "03f37fc2ce2eef16dcea",
+    "url": "/smart-shop-app/static/css/2.1479e602.chunk.css"
+  },
+  {
+    "revision": "03f37fc2ce2eef16dcea",
+    "url": "/smart-shop-app/static/js/2.641c11f9.chunk.js"
+  },
+  {
+    "revision": "20d0bc44a7f226e8db642f9db1b7f420",
+    "url": "/smart-shop-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef87d0502ecde92fa9a76c31231adb1a.js"
+  "/smart-shop-app/precache-manifest.4d44f5d8b8f52c5f52a2716a2508d6c6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/smart-shop-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
